af85c27ac3036b053dd85b7a47c06dc303941256
Gazica <tomislav.gazica@gmail.com>
2020-02-07 10:42:19 +0100

    refactored JSONAPIError

--- Sources/jab/JSONAPIError.swift
+++ Sources/jab/JSONAPIError.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 public struct JSONAPIError: Decodable, Error {
-    struct Source: Decodable {
-        let pointer: String
+    public struct Source: Decodable {
+        public let pointer: String
     }
     
-    let status: String?
-    let source: Source?
-    let title: String?
-    let detail: String?
+    public let status: String?
+    public let source: Source?
+    public let title: String?
+    public let detail: String?
 }
 
 public struct JSONAPIErrors: Decodable, Error {
